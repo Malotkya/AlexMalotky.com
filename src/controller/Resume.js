f9ee7d8c6abe8a06377d85bd645c9f5f75723f24
Malotkya <ajmalotky@madisoncollege.edu>
2020-06-24 14:40:34 -0500

    Added resume page

--- src/controller/Resume.js
+++ src/controller/Resume.js
@@ -3,9 +3,29 @@ class Resume {
         this.path = "/Resume"
     }
 
-    get(req, res) {
-        res.write("This is the resume page!");
-        res.end();
+    async get(req, res) {
+        let SchoolHistory = require('../dao/SchoolHistory.js');
+        let JobHistory = require('../dao/JobHistory.js');
+        let errorMessage = "";
+        let schoolHistory = Array();
+        let jobHistory = Array();
+
+        try {
+            let sqlResponce = await new SchoolHistory().orderBy('graduated', 'DESC').fetchAll();
+            sqlResponce.models.forEach( obj => schoolHistory.push(obj.attributes) );
+
+            sqlResponce = await new JobHistory().orderBy('startDate', 'DESC').fetchAll()
+            sqlResponce.models.forEach( obj => jobHistory.push(obj.attributes) );
+        } catch (error) {
+            console.error(error);
+            errorMessage = JSON.stringify(error);
+        }
+
+        res.render("resume", {
+            schoolHistory:schoolHistory,
+            jobHistory:jobHistory,
+            errMsg:errorMessage
+        });
     }
 
 };
