8d8112cb10bbd85efac03c47994d14cf8780fc3f
Malotkya <ajmalotky@madisoncollege.edu>
2020-07-23 14:37:57 -0500

    Added a signup form

---
+++ src/controller/Signup.js
@@ -0,0 +1,58 @@
+const {hash, compare} = require("../util/password.js");
+
+class Signup {
+    constructor() {
+        this.path = "/Signup";
+    }
+
+    get(req, res) {
+        let callback = req.query.return;
+        res.render("signup", {callback:callback});
+    }
+
+    async post(req, res) {
+        let callback = req.body.callback;
+        let email = req.body.email;
+        let password1 = req.body.password1;
+        let password2 = req.body.password2;
+        let firstName = req.body.firstName;
+        let lastName = req.body.lastName;
+
+        let dao = require("../dao/UserDao.js");
+
+        if(password1 === password2) {
+            let password_hash = hash(password1);
+            let test = await dao.getByUserName(email);
+
+            if(test == null) {
+                let id = await dao.insert(email, password_hash, firstName, lastName);
+                req.session.user = await dao.getById(id);
+                if(callback == "") {
+                    res.redirect("/");
+                } else {
+                    res.redirect(callback);
+                }
+            } else {
+                res.render("signup", {
+                    email:email,
+                    firstName:firstName,
+                    lastName:lastName,
+                    callback:callback,
+                    errMessage:"The email is already taken!"
+                });
+            }
+
+        } else {
+            res.render("signup", {
+                email:email,
+                firstName:firstName,
+                lastName:lastName,
+                callback:callback,
+                errMessage:"The passwords must match!"
+            });
+        }
+
+    }
+};
+
+module.exports = new Signup();
--- src/dao/UserDao.js
+++ src/dao/UserDao.js
@@ -18,6 +18,44 @@ class UserDao {
             return null;
         }
     }
+
+    async getById(id) {
+        try {
+            let responce = await new User({id:id})
+                .fetch({
+                    withRelated: 'roles'
+                });
+
+            let output = responce.attributes;
+            output.roles = Array();
+            responce.relations.roles.models.forEach(obj => {
+                output.roles.push(obj.attributes.type);
+            })
+            return output
+        } catch (error) {
+            return null;
+        }
+    }
+
+    async getAll() {
+        let responce = await new User()>fetchAll({withRelated: 'roles'});
+
+        let output = Array();
+        responce.models.forEach(user => output.push(user));
+
+        return output;
+    }
+
+    async insert(email, password_hash, firstName, lastName) {
+        let user = new User({
+            email:email,
+            password:password_hash,
+            firstName:firstName,
+            lastName:lastName
+        });
+        user = await user.save(null, {method:"insert"});
+        return user.attributes.id;
+    }
 }
 
 module.exports = new UserDao();
