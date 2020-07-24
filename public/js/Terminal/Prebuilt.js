3df10165fdff45d4baa07c33f9fad8b60d52c2c7
Malotkya <ajmalotky@madisoncollege.edu>
2020-07-24 15:46:41 -0500

    removed outdated prebuilt funtions

--- public/js/Terminal/Prebuilt.js
+++ public/js/Terminal/Prebuilt.js
@@ -1,11 +1,7 @@
 export const help = (terminal, args) => {
-    let output = $("<div>").css("margin-left", "4ch");
-
-    for(const key in terminal.functions) {
-        output.append(`${key} - ${terminal.functionDescription[key]}<br/>`);
+    for(const key in terminal.functions.description) {
+        terminal.println(`${key} - ${terminal.functions.description[key]}`);
     }
-
-    terminal.print(output);
 }
 
 export const reset = (terminal, args) => {
@@ -14,20 +10,16 @@ export const reset = (terminal, args) => {
 
 export const exit = (terminal, args) => {
     terminal.println("Good Bie!")
-    window.location.replace("/");
-}
-
-export const print = (terminal, args) => {
-    let string = args.splice(1, args.length-1).join(" ");
-    terminal.print($("<div>").html(string));
+    terminal.close();
 }
 
 export const about = (terminal, args) => {
     terminal.println("This is an attempt to see what I can create in this environement.");
     terminal.println("I plan to continue to expand the functionality of thie terminal");
     terminal.println("Goals Include:");
-    terminal.println("1: Login like functionality")
-    terminal.println("2: Saving settings to cookies");
-    terminal.println("3: Create a file like system in cookies to allow custom apps");
-    terminal.println("4: Create a basic game like snake");
+    terminal.println("[*]: Change how the terminal is displayed")
+    terminal.println("[ ]: Login like functionality")
+    terminal.println("[ ]: Saving settings to cookies");
+    terminal.println("[ ]: Create a file like system in cookies to allow custom apps");
+    terminal.println("[ ]: Create a basic game like snake");
 }
