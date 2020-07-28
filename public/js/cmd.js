24347c30e39994235a9633af5477791f67a593bd
Malotkya <ajmalotky@madisoncollege.edu>
2020-07-28 16:06:33 -0500

    added run call to main function

--- public/js/cmd.js
+++ public/js/cmd.js
@@ -1,8 +1,15 @@
 import Terminal from './Terminal/Terminal.js';
+import App from "./Terminal/App.js"
 
 window.onload = () => {
 
-    let terminal = new Terminal($("#terminal"));
+    let terminal = new Terminal(document.querySelector("#terminal"));
 
     terminal.println("Welcome to AlexMalotky.com");
+
+    terminal.run().catch(e => {
+        console.error(e);
+    });
+
+
 };
