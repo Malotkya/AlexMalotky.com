fe0c5b28b37f667b6d5d71d776aeceebac1b5c8d
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-04 16:18:13 -0500

    Added coments to stream classes

--- public/js/Terminal/System/InputStream.js
+++ public/js/Terminal/System/InputStream.js
@@ -1,12 +1,25 @@
 import Stream from "./Stream.js"
 
+// The input stream waits until it can get data from its stream and also holds the
+// character that will be used as a cursor.
+//
+// @extends: Stream
+// @author Alex Malotky
 class InputStream extends Stream{
+    // Loads the bios so that the stream can wait and set the defualt cursor that
+    // is going to be displayed.
+    //
+    // @param bios
     constructor(bios) {
         super();
         this.bios = bios;
         this.cursor = "█";
     }
 
+    // pulls from the stream untill the characer is met, waits if the character is not
+    // in the stream.
+    //
+    // @param: char to pull till
     get = async (char = " ") => {
         while(true) {
             let index = this.stream.indexOf(char);
@@ -19,6 +32,8 @@ class InputStream extends Stream{
         }
     };
 
+    // pulls from the stream untill the either the newline or return is met.
+    //
     getln = async () => {
         while(true) {
             let n = this.stream.indexOf('\n');
--- public/js/Terminal/System/Stream.js
+++ public/js/Terminal/System/Stream.js
@@ -1,23 +1,44 @@
+// This class acts like a stream to handel inputs and outputs.  This class doesn't
+// really contain an actual stream but instead containts a string that is read from
+// and a buffer that is editable that is then "saved" to the stream string.
+//
+// author: Alex Malotky
 class Stream {
+    //Initializes the stream and buffer strings.
     constructor() {
         this.stream = "";
         this.buffer = "";
     }
 
+    // Appends to the end of the buffer
+    //
+    // @param string to append
     add = string => this.buffer += string;
 
+    // removes the last char from the buffer string
+    //
     remove = () => this.buffer = this.buffer.slice(0, -1);
 
+    // Sets the buffer to the string and forgets old buffer data
+    //
+    // @param string to set the buffer
     set = string => {
         if(typeof string === "string")
             this.buffer = string;
     }
 
+    // Appends the buffer to the stream and clears the buffer data
+    //
+    // @param string to append
     clear = () => {
         this.stream += this.buffer;
         this.buffer = "";
     }
 
+    // Pulls from the stream up to the index, if the index is invalid or the returned
+    // data would be empty for any reason, null is returned instead
+    //
+    // @param index ot pull till
     pull = index => {
         if(index >= 0) {
             let buffer = this.stream.substr(0, index);
