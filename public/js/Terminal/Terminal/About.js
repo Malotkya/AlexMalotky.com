1d5603effaee43586669a05862b1491690dbd8f7
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-04 15:34:07 -0500

    add scrolling to the todo list

--- public/js/Terminal/Terminal/About.js
+++ public/js/Terminal/Terminal/About.js
@@ -9,10 +9,11 @@ class Help extends App {
         system.println("This is an attempt to see what I can create in this environement.");
         system.println("I plan to continue to expand the functionality of thie terminal");
         system.println("Goals Include:");
-        system.println("[*]: Change how the terminal is displayed")
+        system.println("[*]: Change the terminal to be desplayed using 2D Graphics.")
+        system.println("[ ]: Add automatic scrolling functionality")
         system.println("[ ]: Login like functionality")
         system.println("[ ]: Saving settings to cookies");
-        system.println("[ ]: Create a file like system in cookies to allow custom apps");
+        system.println("[ ]: Create a file like system to allow custom apps");
         system.println("[ ]: Create a basic game like snake");
     }
 
