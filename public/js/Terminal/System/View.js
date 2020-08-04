b7d9c7b8c56a82780ea71172e7097efe39d93f06
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-04 15:33:50 -0500

    created a viewport system that renders in a hidden canvas

--- public/js/Terminal/System/View.js
+++ public/js/Terminal/System/View.js
@@ -1,26 +1,77 @@
-import System from "./System.js";
-
 class View {
     constructor(system) {
-        system.bios.view();
-
+        //bios
+        this.top = system.bios.view();
         this.width = system.bios.width;
         this.height = system.bios.height;
+        this.cw = system.bios.cw;
+        this.ch = system.bios.ch;
+
+        this.font = system.bios.font;
+        this.background = system.bios.background;
+
+        //hiden canvas
+        let canvas = document.createElement("canvas");
+        canvas.width = this.width * this.cw;
+        canvas.height = this.height * this.ch
+        canvas.style.zIndex = -1;
+        canvas.style.position = "absolute";
+        canvas.style.top = 0;
+        canvas.style.left = 0;
+
+        this.ctx =canvas.getContext("2d");
+        this.ctx.fillStyle = system.bios.gl.fillStyle;
+        this.ctx.font = system.bios.gl.font;
+
+        document.querySelector("body").appendChild(canvas);
 
         system.view = this;
-        this.arr = Array(this.width).fill(null).map(() => Array(this.height));
+        this.running = true;
+    }
+
+    output = (x,y,string) => {
+        this.ctx.fillStyle = this.font;
+        this.ctx.fillText(string, (x-1)*this.cw, (y*this.ch));
     }
 
     test = () => {
         for(let x=1; x<=this.width; x++) {
             for(let y=1; y<=this.height; y++) {
-                this.arr[x][y] = x+y;
+                this.output(x,y, Math.floor(Math.random() * 10));
             }
         }
     }
 
-    render = system => {
+    clear = async() => {
+        this.ctx.fillStyle = this.background;
+        this.ctx.fillRect( 0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    }
 
+    open = async() => {
+        if(this.running) {
+            await this.sleep();
+            this.clear();
+        }
+        return this.running;
+    }
+
+    render = () => {
+        if(this.running)
+            return this.ctx.getImageData(0,0,this.ctx.canvas.width, this.ctx.canvas.height);
+
+        return this.last;
+    }
+
+    close = () => {
+        this.last = this.ctx.getImageData(0,0,this.ctx.canvas.width, this.ctx.canvas.height);
+        this.running = false;
+    }
+
+    sleep = () => new Promise(r => window.setTimeout(r, 1));
+
+    delete = () => {
+        close();
+        this.ctx.canvas.remove();
     }
 
 
