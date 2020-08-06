e33106e9e02bf97d7a2891373609b7724915663b
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-06 15:14:59 -0500

    back to launching from app.js

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Welcome to (AlexMaloky.com)[https://AlexMalotky.com]
-!!! The website is currently down as I transition it to AWS !!!
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 const express = require("express");
 const helmet = require("helmet");
 const session = require("express-session");
