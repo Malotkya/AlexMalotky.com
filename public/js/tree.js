2671ebc69c1b98805cd3d7448984d46b6a459cce
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-19 12:40:59 -0500

    now incorperates images

--- public/js/tree.js
+++ public/js/tree.js
@@ -1,4 +1,6 @@
 let tree = new Array();
+let width = 0;
+let max = 150;
 
 window.onload = () => {
     let select = document.querySelector("#select");
@@ -36,18 +38,28 @@ const updateAll = () => {
         node.innerText = person.name;
     })
 
+    width = 0;
     updateTree(person);
     updateEdit(person);
     updateDelete(person);
+
+    location.href = `#${select.value}`;
+
+    zoom({target:{
+        localName: "a",
+        id: select.value
+    }});
 }
 
 const updateTree = person => {
     let list = document.createElement("ul");
-    list.appendChild(render(person));
+    let li = render(person);
+    list.appendChild(li);
 
     let target = document.querySelector(".tree");
     target.innerHTML = "";
     target.appendChild(list);
+    list.style.width = `${width}px`;
 }
 
 const updateEdit = person => {
@@ -83,17 +95,24 @@ const updateDelete = person => {
 
 const zoom = event => {
     let target = event.target;
-    if(target.localName != "a")
+    while(target.localName != "a")
         target = target.parentNode;
 
     let id = Number(target.id);
     if( isNaN(id) )
         id = 1;
 
-    let person = tree[id-1];
-    console.log(person);
+    let person = findById(id);
+    let view = document.querySelector("#view");
+    view.innerHTML = "";
 
-    //update picture
+    let img = document.createElement("img");
+    if(person.picture === "")
+        img.src = "/img/Family/none.png";
+    else
+        img.src = "/img/Family/" + person.picture;
+
+    view.appendChild(img);
 }
 
 const findChildren = parent => {
@@ -119,22 +138,33 @@ const render = parent => {
     li.appendChild(span);
 
     let figure = document.createElement("figure");
-    figure.innerText = parent.picture;
+    figure.style.width = `${max-20}px`;
+    figure.style.margin = "auto";
+
+    let img = document.createElement("img");
+    if(parent.picture === "") {
+        img.src = "/img/Family/none.png";
+    }else {
+        img.src = "/img/Family/" + parent.picture;
+    }
+    figure.appendChild(img);
     li.appendChild(figure);
 
     a.appendChild(span);
     a.appendChild(figure);
     a.id = parent.id;
     a.addEventListener("click", zoom)
+    a.style.width = `${max}px`;
 
     li.appendChild(a);
     if(parent.children.length > 0) {
         let ul = document.createElement("ul");
-        parent.children.forEach( child => ul.appendChild(render(child)) );
-
+        parent.children.forEach( child => {
+            ul.appendChild(render(child));
+            width += (max-20);
+        });
         li.appendChild(ul);
     }
-
     return li;
 }
 
