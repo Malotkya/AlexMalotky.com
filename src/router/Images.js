c7610cf3f12bf348377d930593faf59ba52bd083
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-24 11:47:48 -0500

    can now upload images this way and get images in a json list

---
+++ src/router/Images.js
@@ -0,0 +1,50 @@
+let express = require('express');
+let images = express.Router();
+let ImageDao = require("../dao/ImageDao.js");
+
+images.path = "/img";
+
+images.get("/:Dir?", async(req,res) => {
+    try {
+        let dao = new ImageDao(req.params.Dir);
+        res.json(dao.getAll());
+    } catch (e) {
+        if(e.message === "404")
+            res.status(404).json({message:"Page not Found!"});
+
+        res.status(500).json({message:"There was an error!"});
+    }
+
+});
+
+images.post("/:Dir", async(req,res) => {
+    if(req.session.user.roles.includes("Admin")) {
+        try {
+            let dao = new ImageDao(req.params.Dir);
+            dao.upload(req.file.image.name, req.files.image.data,
+                        req.files.image.mimetype);
+        } catch (e) {
+            res.status(500).json(e);
+        }
+        let dao = new ImageDao(req.params.Dir, true);
+        dao.upload(req.file.image.name, req.files.image.data,
+                    req.files.image.mimetype);
+    } else {
+        res.status(500).json({message:"You need to be an admin to do that!"});
+    }
+});
+
+images.delete("/:Dir", async(req,res) => {
+    if(req.session.user.roles.includes("Admin")) {
+        try {
+            let dao = new ImageDao(req.params.Dir);
+            dao.delete(req.file.image.name);
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    } else {
+        res.status(500).json({message:"You need to be an admin to do that!"});
+    }
+});
+
+module.exports = images;
